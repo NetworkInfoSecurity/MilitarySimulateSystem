d9756d6c5af90a80b1af60c61cf3039b7c5de608
chenwhql <sunny_cwh@163.com>
2017-05-16 15:09:38 +0800

    Frame supplement

---
+++ src/thu/infosecurity/simulate/controller/SceneCreator.java
@@ -0,0 +1,42 @@
+package thu.infosecurity.simulate.controller;
+
+/**
+ * Created by ChenWeihang on 2017/5/16 0016.
+ */
+public class SceneCreator {
+
+    /**
+     * 初始化模拟场景需要的各个元素
+     * 比如几个伞兵、几个装备箱、伞兵装备箱的初始坐标等
+     */
+    public void SceneInit(int soldierNum){
+
+    }
+
+    /**
+     * 降落过程模拟
+     * 降落指令发出，伞兵装备箱依次落地
+     */
+    public void landing(){
+
+    }
+
+    /**
+     * 开始模拟整个军事行动过程
+     * - 伞兵开始移动
+     * - 伞兵遇到其他士兵进行认证
+     * - 认证成功选出leader
+     * - 小队跟随leader前进
+     * - 发现装备箱尝试去开启
+     * - 若人数不够，记录装备箱位置继续前进
+     * - 增加到足够人数回到装备箱所在处再次开启
+     */
+    public void SimulateAction(){
+
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}

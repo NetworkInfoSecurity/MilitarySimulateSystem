0b574eb14b5f87696260a8f6b4da8986239f5e04
chenwhql <sunny_cwh@163.com>
2017-05-21 17:22:17 +0800

    加了一小段代码做个测试
    让两个士兵接近的时候验证一下，并在旁边的系统信息框上显示

--- src/thu/infosecurity/simulate/view/MainViewController.java
+++ src/thu/infosecurity/simulate/view/MainViewController.java
@@ -1,5 +1,6 @@
 package thu.infosecurity.simulate.view;
 
+import com.sun.tools.corba.se.idl.Util;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -23,6 +24,8 @@ import thu.infosecurity.simulate.controller.SceneCreator;
 import thu.infosecurity.simulate.controller.SceneInitial;
 import thu.infosecurity.simulate.model.Soldier;
 import thu.infosecurity.simulate.model.Target;
+import thu.infosecurity.simulate.util.RSA;
+import thu.infosecurity.simulate.util.Utils;
 
 import javax.management.timer.*;
 import java.awt.*;
@@ -64,12 +67,22 @@ public class MainViewController {
 
     private boolean isOk = false;    //表示是否点击开始按钮
 
-    private static final double MIN_DISTANCE = 40.0; //表示每个士兵方圆MIN_DISTANCE像素以内表示可以进行认证对话等
+    private static final double MIN_DISTANCE = 50.0; //表示每个士兵方圆MIN_DISTANCE像素以内表示可以进行认证对话等
+
+    private static final double STEP_LENGTH = 10.0; //表示每次上士兵移动的步长
 
     private Set<Integer> arriveTargetSet = new HashSet<Integer>();  //表示已经到达box目的地的士兵id
 
+    private Set<Integer> authSet = new HashSet<>(); //表示已经认证过的士兵集合
+//    private Map<String, Set<Integer>> authMap = new HashMap<>(); //表示已经认证过的士兵集合,String是验证的口令，后面是用有同样口令的集合
+
+    private Soldier soldierLeader = new Soldier();
+
+    //private String plain = "We are family! oh yeah!";  //每个士兵要用于跟别人验证的口令，这个也应该是每个士兵的属性！
+
     private boolean isOpenBox = false;  //表示打开设备箱子
 
+    private Thread refresh;
 
 //    @FXML
 //    private PerspectiveCamera sceneCam;
@@ -82,6 +95,8 @@ public class MainViewController {
         sc = new SceneInitial();
 
         arriveTargetSet.clear();
+        authSet.clear();
+        //authMap.clear();
     }
 
     /**
@@ -90,6 +105,7 @@ public class MainViewController {
      */
     @FXML
     private void initialize() {
+
 //        // Handle Button event.
 //        startBtn.setOnAction((ActionEvent event) -> {
 //            GraphicsContext gc = sceneCanvas.getGraphicsContext2D();
@@ -104,7 +120,7 @@ public class MainViewController {
 
 
         //启动线程，每隔300ms更新goup
-        new Thread() {
+        refresh = new Thread() {
             public void run(){
                 while(true)
                 {
@@ -127,7 +143,8 @@ public class MainViewController {
                 }
             }
 
-        }.start();
+        };
+        refresh.start();
 
     }
 
@@ -151,6 +168,8 @@ public class MainViewController {
         sc = new SceneInitial();
 
         arriveTargetSet.clear();
+        authSet.clear();
+        //authMap.clear();
 
     }
 
@@ -296,7 +315,51 @@ public class MainViewController {
         }
 
         //TODO 添加遍历每个士兵，周围MIN_DISTANCE距离以内表示可以进行认证操作
+        for(int i = 0; i < soldierList.size(); i++){
 
+            //暂时写了两个士兵之间认证的逻辑
+            Soldier sA = soldierList.get(i);
+            for(int j = i + 1; j < soldierList.size(); j++){
+                Soldier sB = soldierList.get(j);
+                if((!authSet.contains(sA.getID()) || !authSet.contains(sB.getID()))&& dis(sA.getPosition(),sB.getPosition()) <= MIN_DISTANCE){  //开始认证
+                    infoTextArea.appendText(sA.getName() + "和" + sB.getName() + "开始认证" + "\r\n");
+                    //验证士兵，但是这样有问题，应该碰到一起再验证，函数的输入应该是两个人
+                    if(RSA.soldierVerify(sA) && RSA.soldierVerify(sB)){
+                        infoTextArea.appendText(sA.getName() + "和" + sB.getName() + "认证成功" + "\r\n");
+                        authSet.add(sA.getID());
+                        authSet.add(sB.getID());
+                    }
+                }
+            }
+
+            /*for(int j = i; j < soldierList.size(); j++){
+                Soldier sB = soldierList.get(j);
+                if(dis(sA.getPosition(),sB.getPosition()) <= MIN_DISTANCE){  //开始认证
+                    try {
+                        refresh.wait();
+                        String info = sA.getName() + "和" + sB.getName() + "开始认证";
+                        infoTextArea.setText(info);
+
+                        //士兵A将其口令作为key创建集合，如果已经存在，则忽略
+                        if(!authSet.containsKey(plain)){
+                            authMap.put(plain, new HashSet<Integer>());
+                            authMap.get(plain).add(sA.getID());
+                        }
+                        //士兵B将自己的口令用A的公钥加密得到密文
+                        String[] puKey = sA.getPuKey().split(",");
+                        String nA = puKey[0];
+                        String eA = puKey[1];
+                        String enStr = RSA.RSA_Encrypt(plain, eA, nA);
+                        //士兵A将密文用自己的私钥解密进行认证
+                        String[] prKey = sA.getPrKey().split(",");
+                        String dA = prKey[1];
+                        String deStr = RSA.RSA_Decrypt(enStr, dA, nA);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }*/
+        }
 
 
 
@@ -341,16 +404,17 @@ public class MainViewController {
 
     /**
      * x向目的地box走一步，返回坐标
-     * @param x
      * @param target
      * @return
      */
     private Point walkOne(Point a, Point target)
     {
-        double verticalDis = MIN_DISTANCE * Math.abs(a.getY()-target.getY()) / dis(a, target);   //竖直的距离差
-        double horizontalDis = MIN_DISTANCE * Math.abs(a.getX()-target.getX()) / dis(a, target);   //水平的距离差
+        double verticalDis = STEP_LENGTH * Math.abs(a.getY()-target.getY()) / dis(a, target);   //竖直的距离差
+        double horizontalDis = STEP_LENGTH * Math.abs(a.getX()-target.getX()) / dis(a, target);   //水平的距离差
 
         Point p = new Point();
+
+//        int orient = Utils.generateRandom(0,1);
 
         //以下在target的八个方位进行计算
         if(a.getX() <= target.getX() && a.getY() <= target.getY())  //左上
@@ -371,6 +435,10 @@ public class MainViewController {
         }
         else if(a.getX() >= target.getX() && a.getY() >= target.getY())  //右下
         {
+//            switch(orient){
+//                case 0: p.setLocation(a.getX() - horizontalDis, a.getY()); break;
+//                case 1: p.setLocation(a.getX(), a.getY() - verticalDis); break;
+//            }
             p.setLocation(a.getX() - horizontalDis, a.getY() - verticalDis);
         }
         else if(a.getX() == target.getX() && a.getY() > target.getY())  //下

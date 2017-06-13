9fa89894f387a2e848243ac633291899964faac1
lixu16 <1668439478@qq.com>
2017-06-13 21:45:33 +0800

    李旭
    返回军衔最高的士兵列表

--- src/thu/infosecurity/simulate/util/Millionaire.java
+++ src/thu/infosecurity/simulate/util/Millionaire.java
@@ -52,43 +52,46 @@ public class Millionaire {
         s3.setPrKey(str6);
         s3.setSecretLevel("T");
         soldiers.add(s3);
+        ArrayList<Soldier> topSoliders = new ArrayList<Soldier>();//顶级军衔列表
 
-        int res = getTopLeader_Million(soldiers);
-        System.out.println(res);
-    }
-/**
-    public static int getTopLeader_Million( ArrayList<Soldier> soldierList){
-        Soldier top_Soldier = soldierList.get(0);
-        for(int i=1;i<soldierList.size();i++){
-            Soldier temp_Soldier = soldierList.get(i);
-            if(agreeLeader(top_Soldier,temp_Soldier)){
-                top_Soldier = temp_Soldier;
-            }
+        topSoliders = getTopLeader_Million(soldiers);
+        for(Soldier s : topSoliders){
+            System.out.println(s.getID());
         }
-        return top_Soldier.getID();
     }
-**/
-    public static int getTopLeader_Million( ArrayList<Soldier> soldierList){
+    /**
+     public static int getTopLeader_Million( ArrayList<Soldier> soldierList){
+     Soldier top_Soldier = soldierList.get(0);
+     for(int i=1;i<soldierList.size();i++){
+     Soldier temp_Soldier = soldierList.get(i);
+     if(agreeLeader(top_Soldier,temp_Soldier)){
+     top_Soldier = temp_Soldier;
+     }
+     }
+     return top_Soldier.getID();
+     }
+     **/
+    public static ArrayList<Soldier> getTopLeader_Million( ArrayList<Soldier> soldierList){
+        ArrayList<Soldier> topSoldiers = new ArrayList<Soldier>();//顶级军衔列表
         Soldier top_Soldier = soldierList.get(0);
+        topSoldiers.add(top_Soldier);
         int count = 1; //表示现在拥有最高级别的士兵个数
         for(int i=1;i<soldierList.size();i++){
             Soldier temp_Soldier = soldierList.get(i);
             if(agreeLeader(top_Soldier,temp_Soldier)){    //当返回值为true时，表明后者级别高
                 if(agreeLeader(temp_Soldier,top_Soldier)){
                     count++;
+                    topSoldiers.add(temp_Soldier);
                 }
                 else{
+                    topSoldiers.clear();
                     top_Soldier = temp_Soldier;
+                    topSoldiers.add(top_Soldier);
                     count = 1;
                 }
             }
         }
-        if(count > 1){
-            return -1;
-        }else
-        {
-            return top_Soldier.getID();
-        }
+        return topSoldiers;
     }
 
 
@@ -120,7 +123,7 @@ public class Millionaire {
         String[] str = puKey.split(",");
         String part1_puKey = str[1];
         String part2_puKey = str[0];
-//        System.out.println("part2_puKey:" + part2_puKey);
+        System.out.println("part2_puKey:" + part2_puKey);
         BigInteger result = encrypt(bigNum, new BigInteger(part1_puKey), new BigInteger(part2_puKey));
         int level_num = Target.toLevel(A.getSecretLevel())+1;//等级从1开始，1、2、3
         BigInteger level = new BigInteger(level_num+"");
@@ -152,7 +155,7 @@ public class Millionaire {
             String part2_prKey = str[0];
             BigInteger temp2 = decrypt(temp1, new BigInteger(part1_prKey), new BigInteger(part2_prKey));
             temp2 = temp2.mod(bigNum);
-//            System.out.println("temp2:"+temp2);
+            System.out.println("temp2:"+temp2);
 
             if( i > 1 + Target.toLevel(B.getSecretLevel()) ){  //把等级值加1
                 temp2 = temp2.add(new BigInteger("1"));
@@ -174,10 +177,10 @@ public class Millionaire {
         BigInteger random_A = new BigInteger(random1);
         BigInteger random_B = new BigInteger(list.get(list.size()-1));
         int result1 = Integer.parseInt(random_A.mod(random_B).toString());
-//        System.out.println("result1=" + result1);
+        System.out.println("result1=" + result1);
         int result2 = Integer.parseInt(list.get( Target.toLevel(A.getSecretLevel())));
         //int result2 = Integer.parseInt(list.get( 1 ));
-//        System.out.println("result2=" + result2);
+        System.out.println("result2=" + result2);
         if(result1 != result2){
             return false;
         }else{
